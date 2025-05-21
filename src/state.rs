8ad4d7b1096c955af90ed5d2ddbde2bfaad56444
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-05-21 13:45:24 +0200

    State.rs

---
+++ src/state.rs
@@ -0,0 +1,23 @@
+#[derive(Clone, Copy)]
+struct Vec2 {
+    x: f32,
+    y: f32,
+}
+
+pub struct PlayerState {
+    pos: Vec2,
+    vel: Vec2,
+    acc: f32,
+    jump_force: f32,
+    jump_timer: f32,
+    color: Color,
+    size: f32,
+}
+
+pub struct GameState {
+    players: Vec<PlayerState>,
+}
+
+impl GameState {
+    pub fn update(&mut self)
+}
