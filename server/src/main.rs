e96fd3d9c77c51b14943e0f8270bce688f7637d3
Embret Roås <embret.roas@outlook.com>
2025-05-26 17:09:40 +0200

    feat: change fullscreen button name

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,4 @@
 use flatbuffers::FlatBufferBuilder;
-use shared::generated::{PlayerCommand, PlayerCommands, PlayerCommandsArgs};
 use shared::state::{CommandContent, GameState, PlayerStateCommand};
 use std::collections::HashMap;
 use std::io;
@@ -11,7 +10,7 @@ use std::thread::sleep;
 use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
 const SCENE_NAME: &str = "scene_3";
-const TICK_DURATION: Duration = Duration::from_millis(1000);
+const TICK_DURATION: Duration = Duration::from_millis(16);
 const SERVER_ADDR: &str = "127.0.0.1:9000";
 
 struct Server {
